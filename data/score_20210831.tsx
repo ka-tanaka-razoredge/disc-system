ef01d454c2ec7969b2979125495717b376d4cc95
ka-tanaka-razoredge <software@razor-edge.net>
2021-09-02 06:43:03 +0900

    1 file modified

--- data/score_20210831.tsx
+++ data/score_20210831.tsx
@@ -11,7 +11,13 @@ export function load() {
         height: 300,
         width: 300,
         discs: [
-          [{ identifier: 'graph', contentsForFrontInner: '<span class="tooltip">graph<span class="tooltiptext tooltip-bottom">√<span style="color: rgba(0, 0, 0, 0.5);">ooGullWing</span> << graph << <span style="color: rgba(0, 0, 0, 0.5);">renderer</span></span>', top: 0, left: 0, title: ''}, { identifier: 'renderer', contentsForFrontInner: '<span style="color: rgba(0, 0, 0, 0.5)">renderer</span>', top: 0, left: 100, title: '' }],
+          [
+            {
+              identifier: 'graph',
+              contentsForFrontInner: '<span class="tooltip">graph<span class="tooltiptext tooltip-bottom">√<span style="color: rgba(0, 0, 0, 0.5);">ooGullWing</span> << graph << <span style="color: rgba(0, 0, 0, 0.5);">renderer</span></span>', top: 0, left: 0, title: ''
+            },
+            { identifier: 'renderer', contentsForFrontInner: '<span style="color: rgba(0, 0, 0, 0.5)">renderer</span>', top: 0, left: 100, title: '' }
+          ],
           [{ identifier: 'defense', contentsForFrontInner: 'defense', top: 0, left: 250, title: ''}],
           [{ identifier: 'd', contentsForFrontInner: 'defense', top: 0, left: 500, title: '' }],
         ]
